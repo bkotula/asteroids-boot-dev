e13dd1250ae34f4ccb4ea16f503b9c8eaed6529d
bkotula <kotula.bartula@gmail.com>
2025-08-23 16:40:32 +0200

    #2 set up a screen

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import pygame
+from pygame import display
+
+from constants import *
+
 def main():
-    print("Hello from asteroids-boot-dev!")
+    pygame.init()
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(color="black")
+        display.flip()
 
 
 if __name__ == "__main__":
